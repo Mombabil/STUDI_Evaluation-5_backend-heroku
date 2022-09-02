53da698d2fec19e1a089c468798f3b561767bd92
Jean-Pierre Ledos <jpledos@gmail.com>
2022-09-02 10:37:36 +0200

    cloudinary config

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: "hcgq4exzi",
+          api_key: "367231743864755",
+          api_secret:":0BjylIpCCg5VIONZzqha9BpUUIo",
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
